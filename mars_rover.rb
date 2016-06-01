65b1520c56b25eeb592e9b871081d21c3aa7ac31
Kshitij Awadhiya <Kshitij@Kshitijs-MacBook-Pro.local>
2016-06-01 14:03:01 +0530

    Check correctly for nil or newline input as exit conditions and check for exceptions

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,5 +1,5 @@
 require './mars_rover_class.rb'
- 
+
 max_coords_input = gets.chomp
 max_coords = max_coords_input.split(" ")
 
@@ -7,7 +7,7 @@ rover = MarsRover.new(max_coords[0].to_i, max_coords[1].to_i)
 
 while true
   current_coords_input = gets
-  break if current_coords_input == "" || current_coords_input == nil
+  break if current_coords_input == "\n" || current_coords_input == nil
   current_coords_input = current_coords_input.chomp
   current_coords = current_coords_input.split(" ")
   cur_x = current_coords[0].to_i
@@ -16,7 +16,12 @@ while true
   rover.set_current_pos(cur_x, cur_y, cur_d)
   
   commands = gets.chomp
-  rover.follow_commands(commands)
+  begin
+    rover.follow_commands(commands)
+  rescue Exception => e
+    puts e.message
+    break
+  end
   
   pos_x = rover.position[:cur_x].to_s
   pos_y = rover.position[:cur_y].to_s
