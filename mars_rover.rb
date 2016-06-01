ce8631af7d1fd9e93caf3557668341b6286bf54a
Kshitij Awadhiya <kawadhiya21@gmail.com>
2016-06-01 13:40:14 +0530

    Check for nil stdin and exit.

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,13 +1,14 @@
 require './mars_rover_class.rb'
-
+ 
 max_coords_input = gets.chomp
 max_coords = max_coords_input.split(" ")
 
 rover = MarsRover.new(max_coords[0].to_i, max_coords[1].to_i)
 
 while true
-  current_coords_input = gets.chomp
-  break if current_coords_input == ""
+  current_coords_input = gets
+  break if current_coords_input == "" || current_coords_input == nil
+  current_coords_input = current_coords_input.chomp
   current_coords = current_coords_input.split(" ")
   cur_x = current_coords[0].to_i
   cur_y = current_coords[1].to_i
